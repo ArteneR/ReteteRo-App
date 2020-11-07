53e59bb70b47b7cd2102296c7c5b5837b521fdc7
Mihai Georgevici <mihai@titlecapture.com>
2020-11-07 19:48:29 +0200

    Test app name env var

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/ReteteRo-App'));
+app.use(express.static(__dirname + '/dist/'+process.env.APP_NAME));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/ReteteRo-App/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/'+process.env.APP_NAME+'/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
