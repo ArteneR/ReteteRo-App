417080be1ea2c02ed98a93e3acab597640421188
Mihai Georgevici <mihai@titlecapture.com>
2020-12-15 20:00:28 +0200

    Added example for handling different screen sizes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { DataService } from '@app/_services/data.service/data.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less', './app.component.tablet.less', './app.component.mobile.less']
 })
 export class AppComponent {
     title = 'ReteteRo-App';
