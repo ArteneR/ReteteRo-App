5c4dcc12bdffd2a90f666703a82ef36031fcff3c
Mihai Georgevici <mihai@titlecapture.com>
2020-12-16 16:54:31 +0200

    Close main nav on click

--- src/app/main.module/main.component/main-nav.component/main-nav.component.ts
+++ src/app/main.module/main.component/main-nav.component/main-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component }        from '@angular/core';
+import { Component, HostListener }        from '@angular/core';
 
 
 @Component({
@@ -7,10 +7,38 @@ import { Component }        from '@angular/core';
   styleUrls: ['./main-nav.component.less', './main-nav.component.mobile.less']
 })
 export class MainNavComponent {
+    inside = false;
+    isMainNavMenuOpened = false;
+
     constructor() { }
 
 
     logout2() {
         alert("Logout2");
+        this.toggleMainNavMenu();
+    }
+
+
+    @HostListener("click") 
+    clicked() {
+        this.inside = true; 
+    } 
+
+
+    @HostListener("document:click") 
+    clickedOut() { 
+        if (this.inside) {
+            console.log("Inside!");
+        }
+        else {
+            console.log("Outside!");
+            this.isMainNavMenuOpened = false;
+        }
+        this.inside = false; 
+    } 
+
+
+    toggleMainNavMenu() {
+        this.isMainNavMenuOpened = !this.isMainNavMenuOpened;
     }
 }
