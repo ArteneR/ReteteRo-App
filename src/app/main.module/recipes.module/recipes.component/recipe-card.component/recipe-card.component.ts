508bfeb2d5cd17bcf5ee5e072f5b0b3ce6d4f30e
Mihai Georgevici <mihai@titlecapture.com>
2020-12-26 18:19:00 +0200

    Small visual tweaks

--- src/app/main.module/recipes.module/recipes.component/recipe-card.component/recipe-card.component.ts
+++ src/app/main.module/recipes.module/recipes.component/recipe-card.component/recipe-card.component.ts
@@ -5,7 +5,7 @@ import { Recipe }                   from '@app/_models/recipe';
 @Component({
   selector: 'app-recipe-card',
   templateUrl: './recipe-card.component.html',
-  styleUrls: ['./recipe-card.component.less', './recipe-card.component.mobile.less']
+  styleUrls: ['./recipe-card.component.less', './recipe-card.component.tablet.less', './recipe-card.component.mobile.less']
 })
 export class RecipeCardComponent implements OnInit {
   @Input() recipe: Recipe;
