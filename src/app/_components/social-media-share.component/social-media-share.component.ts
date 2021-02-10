226485107f6639e17dc151363970c2d85b97c530
Mihai Georgevici <mihai@titlecapture.com>
2021-02-10 20:09:43 +0200

    Finished the social share component graphic

--- src/app/_components/social-media-share.component/social-media-share.component.ts
+++ src/app/_components/social-media-share.component/social-media-share.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'social-media-share',
   templateUrl: './social-media-share.component.html',
-  styleUrls: ['./social-media-share.component.less']
+  styleUrls: ['./social-media-share.component.less', 'social-media-share.component.mobile.less']
 })
 export class SocialMediaShareComponent { }
--- src/app/_modules/social-media-share.module/social-media-share.module.ts
+++ src/app/_modules/social-media-share.module/social-media-share.module.ts
@@ -1,11 +1,15 @@
 import { NgModule }                 from '@angular/core';
 
+import { AngularSvgIconModule }     from 'angular-svg-icon';
 import { SocialMediaShareComponent} from '@app/_components/social-media-share.component/social-media-share.component';
 
 
 @NgModule({
     declarations: [
         SocialMediaShareComponent,
+    ],
+    imports: [
+        AngularSvgIconModule
     ],
     exports: [
         SocialMediaShareComponent,
