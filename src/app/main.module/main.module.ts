ae933fe70fefc7a463d91b970d19e952b5854654
Mihai Georgevici <mihai@titlecapture.com>
2021-02-13 20:59:01 +0200

    Added add recipe button

--- src/app/main.module/main.module.ts
+++ src/app/main.module/main.module.ts
@@ -1,10 +1,12 @@
-import { NgModule }           from '@angular/core';
-import { CommonModule }       from '@angular/common';
+import { NgModule }              from '@angular/core';
+import { CommonModule }          from '@angular/common';
 
-import { BreadcrumbsModule }  from '@app/_modules/breadcrumbs.module/breadcrumbs.module';
-import { MainComponent }      from '@app/main.module/main.component/main.component';
-import { MainNavComponent }   from '@app/main.module/main.component/main-nav.component/main-nav.component';
-import { MainRoutingModule }  from '@app/main.module/main-routing.module';
+import { AngularSvgIconModule }  from 'angular-svg-icon';
+import { AngularMaterialModule } from '@app/_core/angular-material.module.ts';
+import { BreadcrumbsModule }     from '@app/_modules/breadcrumbs.module/breadcrumbs.module';
+import { MainComponent }         from '@app/main.module/main.component/main.component';
+import { MainNavComponent }      from '@app/main.module/main.component/main-nav.component/main-nav.component';
+import { MainRoutingModule }     from '@app/main.module/main-routing.module';
 
 
 @NgModule({
@@ -14,6 +16,8 @@ import { MainRoutingModule }  from '@app/main.module/main-routing.module';
   ],
   imports: [
     CommonModule,
+    AngularSvgIconModule,
+    AngularMaterialModule,
     BreadcrumbsModule,
     MainRoutingModule
   ],
